7c02d85668db8b84bff2d8b0ea3b02af37b40637
lianmingjie <1137061634@qq.com>
2020-06-01 19:32:25 +0800

    只需要修改YAML配置文件，运行Python程序即可完成个人足迹地图。

--- china_map.py
+++ china_map.py
@@ -11,18 +11,32 @@ from pyecharts import options as opts
 with open("travel_config.yml", 'r', encoding='utf-8') as ymlfile:
     province_city_dict = yaml.safe_load(ymlfile)
 
-province_dict = dict(zip(province_city_dict.keys(), [1]*len(province_city_dict.keys())))
+province_dict = dict(zip(province_city_dict.keys(), [len(_) for _ in province_city_dict.values()]))
 print(province_dict)
 
 # maptype='china' 只显示全国直辖市和省级
-map = Map(init_opts=opts.InitOpts(width='1200px', height='800px'))
+# 指定chart_id，便于后续添加JS function
+map = Map(init_opts=opts.InitOpts(chart_id="123", width='1200px', height='800px'))
 map.set_global_opts(
     title_opts=opts.TitleOpts(title="个人足迹地图"),
-    visualmap_opts=opts.VisualMapOpts(max_=1, is_piecewise=True,
-                                      pieces=[
-                                        {"max": 1, "min": 1, "label": "去过", "color": "#4EA397"},
-                                        {"max": 0, "min": 0, "label": "未去过", "color": "#FFFFFF"},
-                                        ])  #最大数据范围，分段
-                                     )
+    visualmap_opts=opts.VisualMapOpts(max_=20, is_piecewise=True)
+)
+
 map.add("个人足迹地图", data_pair=province_dict.items(), maptype="china", is_roam=True)
+# 添加JS function
+js_func = """
+        chart_123.on('click', function (param){
+            var selected = param.name;
+                if (selected) {
+                    switch(selected){
+          """
+
+for province, cities in province_city_dict.items():
+    js_func += '\t\t\t\t\t\tcase "%s":\n\t\t\t\t\t\t\tlocation.href = "provinces/%s.html";' \
+               '\n\t\t\t\t\t\t\tbreak;\n' % (province, province)
+
+js_func += "\t\t\t\t\t\tdefault:\n\t\t\t\t\t\t\tbreak;}}});"
+print(js_func)
+map.add_js_funcs(js_func)
+
 map.render('全国.html')
